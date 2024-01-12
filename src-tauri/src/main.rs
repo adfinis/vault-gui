fc170f95e4756176c7907ca0af60e8dcfa3f91d4
Gian Klug <gian.klug@adfinis.com>
2024-01-12 20:41:30 +0100

    feat: first backend implementation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,85 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::collections::HashMap;
+use vaultrs::client::{VaultClient, VaultClientSettingsBuilder};
+use vaultrs::{kv2, sys};
+
+fn vault_login(address: String, token: String) -> VaultClient {
+    let settings = VaultClientSettingsBuilder::default()
+        .address(&address)
+        .token(&token)
+        .build();
+    let client = match settings {
+        Ok(settings) => settings,
+        Err(e) => {
+            panic!("Error creating settings: {}", e);
+        }
+    };
+
+    let client = VaultClient::new(client);
+    match client {
+        Ok(client) => {
+            return client;
+        }
+        Err(e) => {
+            panic!("Error creating client: {}", e);
+        }
+    }
+}
+
+#[tauri::command]
+async fn list_kvs(address: String, token: String) -> Vec<String> {
+    let client = vault_login(address, token);
+    let result = sys::mount::list(&client).await;
+    match result {
+        Ok(list) => {
+            // Only return the string parts of the HashMap
+            return list.keys().cloned().collect();
+        }
+        Err(e) => {
+            panic!("Error listing keys: {}", e);
+        }
+    }
+}
+
+#[tauri::command]
+async fn list_path(address: String, token: String, mount: String, path: String) -> Vec<String> {
+    let client = vault_login(address, token);
+    let result = kv2::list(&client, &mount, &path).await;
+    match result {
+        Ok(list) => {
+            return list;
+        }
+        Err(e) => {
+            panic!("Error listing keys: {}", e);
+        }
+    }
+}
+
+#[tauri::command]
+async fn get_secret(address: String, token: String, mount: String, path: String) -> HashMap<String, String> {
+    let client = vault_login(address, token);
+    let result = kv2::read(&client, &mount, &path).await;
+    match result {
+        Ok(secret) => {
+            return secret;
+        }
+        Err(e) => {
+            panic!("Error reading secret: {}", e);
+        }
+    }
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            list_kvs,
+            list_path,
+            get_secret
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import type { Component } from 'solid-js'
+import { invoke } from '@tauri-apps/api/tauri'
+import { createSignal, type Component } from 'solid-js'
 import vault from './assets/vault-logo.svg'
 
 const App: Component = () => {
+  const [url, setUrl] = createSignal('')
+  const [token, setToken] = createSignal('')
+  const [kvs, setKvs] = createSignal({})
+  const showKvs = () => {
+    ;(async () => {
+      try {
+        // Directly await the promise and set the result
+        const data = await invoke('list_kvs', { address: url(), token: token() })
+        setKvs(data)
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  }
+
   return (
     <>
       <header class="supports-backdrop-blur:bg-background/60 bg-background/95 bg-black sticky top-0 z-40 w-full border-b backdrop-blur">
@@ -22,10 +38,45 @@ const App: Component = () => {
           </div>
         </div>
       </header>
-      <div class="flex mx-5 my-2">
-        <nav class="w-1/4">Tree here</nav>
-        <main class="w-full">Ui here</main>
-      </div>
+      <div class="flex mx-5 my-2">Tree here</div>
+      <main class="w-full">
+        <div class="p-4">
+          <div class="mb-4">
+            <label for="url" class="block text-sm font-medium text-gray-700">
+              URL
+            </label>
+            <input
+              id="url"
+              type="text"
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              onInput={e => setUrl(e.currentTarget.value)}
+            />
+          </div>
+
+          <div class="mb-4">
+            <label for="token" class="block text-sm font-medium text-gray-700">
+              Token
+            </label>
+            <input
+              id="token"
+              type="text"
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              onInput={e => setToken(e.currentTarget.value)}
+            />
+          </div>
+
+          <button
+            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={showKvs}
+          >
+            Show KVs
+          </button>
+
+          <div class="mt-4">
+            <pre>{JSON.stringify(kvs(), null, 2)}</pre>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
