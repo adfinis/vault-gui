3149236729eee8a308e082d0d8e80d15a2ea03d3
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-01-12 15:11:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VaultGUI
+
+GUI for Vault made with solid.js and tauri
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'class', // or 'media'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
