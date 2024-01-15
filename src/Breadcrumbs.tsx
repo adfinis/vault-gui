87c22445ca49983c9278265b0a34cf6b6f10c373
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-01-15 17:33:15 +0100

    refactor: solidify

--- src/Breadcrumbs.tsx
+++ src/Breadcrumbs.tsx
@@ -1,39 +1,31 @@
+import { For } from 'solid-js'
 import { useAppContext } from './context'
 
 const Breadcrumbs = (props: { page: string; kv: string; path: string }) => {
   const { updateContext } = useAppContext()
-
-  const handleClick = (index: number) => {
+  console.log(props.path.split('/'))
+  const handleClick = (segment: string) => {
     // Slice the path up to and including the clicked segment
-    const newPath =
-      props.path
-        .split('/')
-        .slice(0, index + 1)
-        .join('/') + '/'
-    updateContext({ page: 'list', kv: props.kv, path: newPath })
-  }
-
-  const handleKvClick = () => {
-    updateContext({ page: 'list', kv: props.kv, path: '' })
+    const path = props.path
+      .split('/')
+      .slice(0, props.path.split('/').indexOf(segment) + 1)
+      .join('/')
+    updateContext({ page: 'list', path })
   }
 
   return (
     <div>
-      <a href="#" onClick={handleKvClick}>
-        {props.kv}
-      </a>
-      {props.path.split('/').map((segment, index, array) => (
-        <span>
-          {index < array.length - 1 ? (
-            // Making each segment clickable, except the last one
-            <a href="#" onClick={() => handleClick(index)}>
-              {segment}/
-            </a>
-          ) : (
-            <span>{segment}</span>
-          )}
-        </span>
-      ))}
+      <span onClick={() => updateContext({ page: 'list', path: '' })}>{props.kv}</span>
+      <For each={props.path.split('/').filter(s => s)}>
+        {segment => (
+          <span
+            class="[&:not(:last-of-type)]:cursor-pointer"
+            onClick={() => handleClick(segment)}
+          >
+            {segment}/
+          </span>
+        )}
+      </For>
     </div>
   )
 }
