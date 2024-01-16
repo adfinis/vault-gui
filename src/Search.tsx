ddc15e8ac4ec7d36817f9eaf3f24428396c91c48
Gian Klug <gian.klug@adfinis.com>
2024-01-16 12:34:21 +0100

    feat: Add filtering for list view

--- src/Search.tsx
+++ src/Search.tsx
@@ -48,7 +48,7 @@ const Search: Component = () => {
           <input
             type="text"
             value={search()}
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black-500 focus:border-black-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-white-500 dark:focus:border-white-500"
             oninput={e => setSearch(e.currentTarget.value)}
             placeholder="Search..."
           />
