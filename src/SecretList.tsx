a4bf6fb3d63065c93dad8c0f0011a7933608b73b
Gian Klug <gian.klug@adfinis.com>
2024-01-17 08:59:38 +0100

    fix: Clear filter on path switch

--- src/SecretList.tsx
+++ src/SecretList.tsx
@@ -1,5 +1,5 @@
 import { document as documentIcon, folder } from 'solid-heroicons/outline';
-import { createResource, createSignal, For, Show, type Component } from 'solid-js';
+import { createEffect, createResource, createSignal, For, Show, type Component } from 'solid-js';
 import Item from './Item';
 import { fetchPaths } from './utils';
 import { state } from './state';
@@ -10,7 +10,17 @@ const ListView: Component = () => {
         fetchPaths,
     );
 
+
+
     const [search, setSearch] = createSignal('');
+
+
+    // Cler the filter field on path change
+    createEffect(() => {
+        if (state.path) {
+            setSearch('');
+        }
+    });
 
     return (
         <div class="p-5">
